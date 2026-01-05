c1108841d2fc5bbcdde67508efe6396b505b9042
Bartosz Chudziński <bartoszchudzinski95@gmail.com>
2026-01-05 19:12:45 +0100

    type hint fix

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -1,3 +1,6 @@
+from app.people.cinema_staff import Cleaner
+
+
 class CinemaHall:
     def __init__(self, number: int) -> None:
         self.number = number
@@ -6,7 +9,7 @@ class CinemaHall:
         self,
         movie_name: str,
         customers: list,
-        cleaning_staff: str,
+        cleaning_staff: Cleaner
     ) -> None:
         print(f'"{movie_name}" started in hall number {self.number}.')
 
