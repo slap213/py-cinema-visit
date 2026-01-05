ae81aab86344e27d9da6f8be52427336ad07550b
Bartosz Chudziński <bartoszchudzinski95@gmail.com>
2026-01-05 19:08:47 +0100

    solution fixed

--- app/cinema/bar.py
+++ app/cinema/bar.py
@@ -1,4 +1,13 @@
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from app.people.customer import Customer
+
+
 class CinemaBar:
     @staticmethod
-    def sell_product(product: str, customer: str) -> None:
+    def sell_product(
+        product: str,
+        customer: "Customer"
+    ) -> None:
         print(f"Cinema bar sold {product} to {customer.name}.")
--- main.py
+++ main.py
@@ -4,16 +4,27 @@ from app.people.customer import Customer
 from app.people.cinema_staff import Cleaner
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str) -> None:
-    customer_instances = [Customer(c["name"], c["food"]) for c in customers]
+def cinema_visit(
+    customers: list,
+    hall_number: int,
+    cleaner: str,
+    movie: str
+) -> None:
+    customer_instances = [
+        Customer(name=c["name"], food=c["food"])
+        for c in customers
+    ]
 
 
     for customer in customer_instances:
-        CinemaBar.sell_product(customer.food, customer)
+        CinemaBar.sell_product(product=customer.food, customer=customer)
 
 
-    staff = Cleaner(cleaner)
+    staff = Cleaner(name=cleaner)
     hall = CinemaHall(number=hall_number)
 
-
-    hall.movie_session(movie, customer_instances, staff)
+    hall.movie_session(
+        movie_name=movie,
+        customers=customer_instances,
+        cleaning_staff=staff
+    )
